70a145920f4c739c0aee5719d887776520096e5d
anupama <anupamakdevops@gmail.com>
2021-10-25 06:48:27 +0000

    updated

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -61,11 +61,11 @@ public class Calculator extends HttpServlet {
         }
     }
         public int add(int a, int b) {
-            return a + b + 1 ;
+            return a + b ;
         }
         
 	    public int sub(int a, int b) {
-            return a - b;
+            return a - b ;
         }
 	    
     	public int mul(int a, int b) {
